0578de0472ce9c7f1ef58d916ebea59a33a8f8b1
Furkan Can Dursun <115872199+DursunFC@users.noreply.github.com>
2025-01-17 09:49:24 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,8 @@
+
+public class main{
+    public static void main(String[] args){
+        student student1 = new student(40, 50, 50);
+        System.out.println(student1.studentId);
+        }
+    }
+
